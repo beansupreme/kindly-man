9615ebbefff67ebf97f8d157fdf113c9c1ea110d
Andy Whillans <andy.whillans@gmail.com>
2015-05-08 16:01:48 -0600

    Remove obsolete .erb templates, move facts_controller to being JSON-only

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -18,51 +18,28 @@ class FactsController < ApplicationController
     render :json => json_attributes(@fact)
   end
 
-  def new
-    @fact = Fact.new
-  end
-
   def create
     @fact = Fact.new(fact_params)
     if @fact.save
-      respond_to do |format|
-        format.html { redirect_to facts_path }
-        format.json { render :json => json_attributes(@fact) }
-      end
+      render :json => json_attributes(@fact)
     else
-      respond_to do |format|
-        format.html { render 'new' }
-        format.json { render :json => {errors: @fact.errors}, status: :bad_request }
-      end
+      render :json => {errors: @fact.errors}, status: :bad_request
     end
-  end
-
-  def edit
-    @fact = Fact.find params[:id]
   end
 
   def update
     @fact = Fact.find params[:id]
     if @fact.update(fact_params)
-      respond_to do |format|
-        format.html { redirect_to facts_path }
-        format.json { render :json => json_attributes(@fact) }
-      end
+      render :json => json_attributes(@fact)
     else
-      respond_to do |format|
-        format.html { render 'edit' }
-        format.json { render :json => {errors: @fact.errors}, status: :bad_request }
-      end
+      render :json => {errors: @fact.errors}, status: :bad_request
     end
   end
 
   def destroy
     @fact = Fact.find params[:id]
     @fact.destroy
-    respond_to do |format|
-      format.html { redirect_to facts_path }
-      format.json { render :json => {}, status: :ok }
-    end
+    render :json => {}, status: :ok
   end
 
   private
--- spec/controllers/facts_controller_spec.rb
+++ spec/controllers/facts_controller_spec.rb
@@ -1,32 +1,59 @@
 require 'rails_helper'
 
 describe FactsController do
-  let(:fact) {Fact.create!(title: 'Ambient Music', subject: 'Can help you study')}
+  let!(:leukemia_fact) { Fact.create!(title: 'Leukemia Treatment', subject: 'Is now being treated with a modified HIV strain') }
+  let!(:cancer_fact) { Fact.create!(title: 'Cancer Treatment', subject: 'Is focusing on using modified viruses to attack tumors') }
 
   describe 'GET #index' do
     it 'returns 200' do
-      get :index
+      xhr :get, :index, format: :json
       expect(response.code).to eq('200')
     end
 
-    it 'assigns all of the facts' do
-      vietnam_fact = Fact.create!(title: 'Saigon', subject: 'Was evacuated in April 1975')
-      cambodia_fact = Fact.create!(title: 'Cambodia', subject: 'Cooperated with North Vietnamese soldiers')
-      get :index
+    it 'returns all of the facts' do
+      xhr :get, :index, format: :json
+      body = JSON.parse(response.body)
 
-      expect(assigns(:facts)).to match_array([vietnam_fact, cambodia_fact])
+      expect(body).to eq([
+                             {
+                                 'id' => leukemia_fact.id,
+                                 'title' => 'Leukemia Treatment',
+                                 'subject' =>'Is now being treated with a modified HIV strain'
+                             },
+                             {
+                                 'id' => cancer_fact.id,
+                                 'title' => 'Cancer Treatment',
+                                 'subject' => 'Is focusing on using modified viruses to attack tumors'
+                             }
+                         ])
     end
   end
 
-  describe 'GET #new' do
-    it 'returns 200' do
-      get :new
-      expect(response.code).to eq('200')
+  describe 'GET #show' do
+    context 'when the fact exists' do
+      before do
+        xhr :get, :show, id: leukemia_fact.id, format: :json
+      end
+
+      it 'returns 200' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns the fact' do
+        results = JSON.parse(response.body)
+        expect(results).to eq({
+                                  'id' => leukemia_fact.id,
+                                  'title' => 'Leukemia Treatment',
+                                  'subject' => 'Is now being treated with a modified HIV strain'
+                              })
+      end
     end
 
-    it 'initializes a fact' do
-      get :new
-      expect(assigns(:fact)).to be_a(Fact)
+    context 'when the fact does not exist' do
+      it 'returns 404' do
+        xhr :get, :show, id: 42, format: :json
+        expect(response.status).to be(404)
+      end
     end
   end
 
@@ -34,59 +61,82 @@ describe FactsController do
     context 'with valid parameters' do
       it 'can create a new fact' do
         fact_params = {title: 'Kombucha', subject: 'Has scoby'}
-        expect(Fact).to receive(:new).with(fact_params).and_return(Fact.new)
-        post :create, fact: fact_params
+        xhr :post, :create, format: :json, fact: fact_params
+
+        expect(Fact.find_by_title('Kombucha')).to be_present
       end
 
-      it 'redirects to the fact path' do
-        post :create, fact: {title: 'Tendonitis', subject: 'is annoying'}
-        expect(response).to redirect_to(facts_path)
+      it 'returns the JSON body of the new fact' do
+        fact_params = {title: 'Radiohead', subject: 'Is the greatest band in the world'}
+        xhr :post, :create, format: :json, fact: fact_params
+
+        expect(response.status).to be(200)
+        body = JSON.parse(response.body)
+
+        expect(body).to eq(
+                            'id' => Fact.last.id,
+                            'title' => 'Radiohead',
+                            'subject' => 'Is the greatest band in the world'
+                        )
+
       end
+
     end
 
     context 'with invalid parameters' do
-      it 're-renders the new page' do
-        post :create, fact: {title: '', subject: ''}
-        expect(response).to render_template('new')
-      end
-    end
-  end
+      it 'returns 400 and errors' do
+        xhr :post, :create, format: :json, fact: {title: 'Blank subject', subject: ''}
+        expect(response.status).to be(400)
 
-  describe 'GET #edit' do
-    it 'can edit an existing fact' do
-      get :edit, id: fact.id
-      expect(assigns(:fact)).to eq(fact)
+        body = JSON.parse(response.body)
+
+        expect(body['errors']).to eq({
+                                         'subject' => ["can't be blank"]
+                                     })
+      end
     end
   end
 
   describe 'PUT #update' do
     it 'can update an existing fact' do
-      put :update, id: fact.id, fact: {title: 'Tomato Sauce', subject: 'contains tomatoes'}
-      updated_fact = assigns(:fact)
+      xhr :put, :update, id: cancer_fact.id, format: :json, fact: {title: 'Tomato Sauce', subject: 'contains tomatoes'}
+      updated_fact = Fact.find cancer_fact.id
       expect(updated_fact.title).to eq('Tomato Sauce')
       expect(updated_fact.subject).to eq('contains tomatoes')
     end
 
-    it 'redirects to the index page' do
-      put :update, id: fact.id, fact: {title: 'Tomato Sauce', subject: 'contains tomatoes'}
-      expect(response).to redirect_to facts_path
+    it 'returns the json body of the fact' do
+      xhr :put, :update, id: leukemia_fact.id, format: :json, fact: {subject: 'Is being researched at University of Penn'}
+
+      expect(response.status).to be(200)
+      body = JSON.parse(response.body)
+
+      expect(body).to eq({
+                             'id' => leukemia_fact.id,
+                             'title' => 'Leukemia Treatment',
+                             'subject' => 'Is being researched at University of Penn'
+                         })
     end
 
-    it 'renders edit if there is an error updating the fact' do
-      put :update, id: fact.id, fact: {title: ''}
-      expect(response).to render_template('edit')
+    it 'returns a 400 with a bad request' do
+      invalid_params = {subject: ''}
+      xhr :put, :update, id: leukemia_fact.id, format: :json, fact: invalid_params
+
+      expect(response.status).to be(400)
+      body = JSON.parse(response.body)
+
+      expect(body['errors']).to eq({
+                                       'subject' => ["can't be blank"]
+                                   })
     end
   end
 
   describe 'DELETE #destroy' do
-    it 'can destroy an existing fact' do
-      delete :destroy, id: fact.id
-      expect(Fact.count).to eq(0)
-    end
+    it 'destroys an existing fact' do
+      xhr :delete, :destroy, id: leukemia_fact.id, format: :json
 
-    it 'redirects to the facts index' do
-      delete :destroy, id: fact.id
-      expect(response).to redirect_to facts_path
+      expect(response.status).to be(200)
+      expect(Fact.find_by_id(leukemia_fact.id)).to be_nil
     end
   end
 end
